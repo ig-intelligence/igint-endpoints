c40cbffca16ac165923f1cca1aa4bde626cc9e36
robin-lee <robin.lee.jw@gmail.com>
2017-01-22 12:17:17 +0800

    update static again

--- static/main.js
+++ static/main.js
@@ -1,14 +1,29 @@
 $("#judgeButton").click(function(){
-    var username = $("#input-7").val();
-    if(username == "") {
-      return;
+  var username = $("#input-7").val();
+
+  if(username == "") {
+    return;
+  }
+
+  $(".loader").show();
+  var url = "/intel/"+username;
+  $.ajax({url: url, success: function(result){
+
+    console.log(result);
+
+    var jsonData = JSON.parse(result);
+    var htmlString = "";
+    console.log(jsonData);
+    for (var i = 0; i < jsonData.length; i++) {
+      var item = "<p>"+jsonData[i]+"</p>";
+      htmlString += item;
     }
-    $(".loader").show();
-    $.ajax({url: "/"+username, success: function(result){
-      console.log(result);
-    }, error: function(err) {
-      $(".loader").hide();
-    }, complete: function() {
-      $(".loader").hide();
-    }});
+
+    $(".results").html(htmlString);
+
+  }, error: function(err) {
+    $(".loader").hide();
+  }, complete: function() {
+    $(".loader").hide();
+  }});
 });
